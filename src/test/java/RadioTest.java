dd27c51d99d16ad7239078079fc508b6ee57db21
Mike Lebedev <to-mike@ya.ru>
2023-03-24 20:12:48 +0300

    coverage added

---
+++ src/test/java/RadioTest.java
@@ -0,0 +1,99 @@
+// package ru.netology.radio;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+public class RadioTest {
+
+    @Test
+    public void testSetNormalStation() {
+        Radio radio = new Radio();
+
+        radio.setCurrentStation(4);
+
+        int expected = 4;
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetMaxOverOneStation() {
+        Radio radio = new Radio();
+
+        radio.setCurrentStation(10);
+
+        int expected = 0;
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetNegativeStation() {
+        Radio radio = new Radio();
+
+        radio.setCurrentStation(-4);
+
+        int expected = 0;
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testNexStation() {
+        Radio radio = new Radio ();
+        radio.setCurrentStation(4);
+
+        radio.next();
+
+        int expected = 5;
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected , actual);
+    }
+
+    @Test
+    public void testNexMaxStation() {
+        Radio radio = new Radio ();
+        radio.setCurrentStation(9);
+
+        radio.next();
+
+        int expected = 0;
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected , actual);
+    }
+
+    @Test
+    public void testPrevMinStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(0);
+
+        radio.prev();
+
+        int expected = 9;
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected , actual);
+    }
+
+    @Test
+    public void testPrevStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(5);
+
+        radio.prev();
+
+        int expected = 4;
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected , actual);
+    }
+
+    @Test
+    public void testSetNormVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(5);
+
+        int expected = 5;
+        int actual = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+}
+
+
